7aaad6716e92e6195888108c73dac0dc5c35cbbe
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-06-22 21:07:54 -0400

    create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client';
+
+import "./index.scss";
+
+//main component
+const MyFlixApplication = () => {
+    return (
+        <div className="my-flix">
+          <div>Good morning</div>
+        </div>
+    );
+};
+
+//finds root of app
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+//tells react how to render app in root DOM element
+root.render(<MyFlixApplication />);
