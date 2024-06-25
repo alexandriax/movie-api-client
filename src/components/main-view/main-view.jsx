3bd88aa5efd56c8a0b5e9bffadcf6d5e3e660f6b
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-06-25 19:59:31 -0400

    create main view component /src.../main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
     const [movies, setMovies] = useState([
         { 
             id: 1, 
@@ -45,3 +47,30 @@ import { MovieView } from "../movie-view/movie-view";
             }
         }
     ]);
+
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    if(selectedMovie) {
+        return (
+        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        );
+    }
+
+    if (movies.length === 0) {
+        return <div>the list is empty!</div>;
+    }
+
+    return (
+        <div>
+            {movies.map((movie) => (
+                <MovieCard 
+                  key={movie.id} 
+                  movie={movie} 
+                  onMovieClick={(newSelectedMovie) => {
+                    setSelectedMovie(newSelectedMovie);
+                  }}
+                  />
+            ))}
+        </div>
+    );
+};
