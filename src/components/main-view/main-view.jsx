3b2f81192561239d3c015502957fa353f3bb21c8
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-06-29 19:57:50 -0400

    import prop types .../main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
