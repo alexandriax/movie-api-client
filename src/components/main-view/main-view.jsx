c765b4daf752df77233bb06469680e5520aa7824
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-06-29 20:01:23 -0400

    connect to api .../main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
 import { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        fetch('https://moo-movies-10a7ea08abc9.herokuapp.com/movies')
+          .then((response) => response.json())
+          .then((data) => {
+            const moviesFromApi = data.docs.map ((doc) => {
+                return {
+                    id: doc.key,
+                    title: doc.title,
+                    image: '',
+                    author: doc.author_name?.[0]
+                };
+            });
+
+            setMovies(moviesFromApi);
+          });
+    }, []);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
