2cb6b14896af66f4fae63884f1b661a32517c23b
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-06-29 20:10:08 -0400

    define prop types .../movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -9,3 +9,21 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       >{movie.title}</div>
     );
 };
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }).isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
