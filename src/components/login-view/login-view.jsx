21723d82ec9ef61bc8cf92d524c0eb62210451f1
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-07-09 21:22:56 -0400

    update code .../login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,8 +9,8 @@ export const LoginView = ({ onLoggedIn }) => {
         event.preventDefault();
 
         const data = {
-            access: username,
-            secret: password
+            Username: username,
+            Password: password
         };
 
         fetch("https://moo-movies-10a7ea08abc9.herokuapp.com/login", {
