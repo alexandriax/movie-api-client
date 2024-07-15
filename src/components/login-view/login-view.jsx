526c30e5b4a8810159404ef365b7db145fcfe39f
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-07-14 20:41:25 -0400

    add local storage .../login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -22,6 +22,8 @@ export const LoginView = ({ onLoggedIn }) => {
         .then((data) => {
             console.log("login response: ", data);
             if (data.user) {
+                localStorage.setItem("user", JSON.stringify(data.user));
+                localStorage.setItem("token", data.token);
                 onLoggedIn(data.user, data.token);
             } else {
                 alert("no such user!");
