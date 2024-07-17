e2d1ca7ce954002c42ab85877281a1c86c136947
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-07-16 20:24:22 -0400

    update moview-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,12 +1,31 @@
 import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 
-export const MovieView = ({ movie }) => {
+
+export const MovieView = ({ movies, user, token, onFavoriteToggle }) => {
     const { movieId } = useParams();
+    const movie = movies.find(m => m.id === movieId);
 
     if (!movie) {
         return <div>Movie not found</div>;
     }
+
+    const isFavorite = user.FavoriteMovies.includes(movie.id);
+
+    const handleFavorite = () => {
+        fetch(`https://moo-movies-10a7ea08abc9.herokuapp.com/users/${user.Username}/movies/${movie.id}`, {
+            method: isFavorite ? 'DELETE' : 'POST',
+            headers: { Authorization: `Bearer ${token}`}
+        })
+        .then(response => response.json())
+        .then(updatedUser => {
+            localStorage.setItem('user', JSON.stringify(updatedUser));
+            onFavoriteToggle(updatedUser);
+        })
+        .catch(e => console.error(e));
+    };
+
     return (
         <div>
             <div>
@@ -28,22 +47,23 @@ export const MovieView = ({ movie }) => {
                 <span>Genre: </span>
                 <span>{movie.genre}</span>
             </div>
-            <Link to="/">Back</Link>
+            <Link to={`/`}>
+                <Button className="back-button">Back</Button>
+            </Link>
+            <Button variant={isFavorite ? 'danger' : 'primary'} onClick={handleFavorite}>
+                {isFavorite ? 'Unfavorite' : 'Favorite'}
+            </Button>
         </div>
     );
-
 };
 
 MovieView.propTypes = {
-    movie: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        image: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        director: PropTypes.string.isRequired,
-        genre: PropTypes.string.isRequired,
-    }).isRequired
+    movies: PropTypes.array.isRequired,
+    user: PropTypes.object.isRequired,
+    token: PropTypes.string.isRequired,
+    onFavoriteToggle: PropTypes.func.isRequired
 };
+
 
 
 
