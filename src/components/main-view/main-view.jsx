0c1239e29d66c450029ef09ad1b852a844604e8f
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-07-16 20:19:52 -0400

    add nav bar .../main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,6 +7,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
@@ -48,6 +49,7 @@ export const MainView = () => {
 
     return (
         <BrowserRouter>
+            <NavigationBar user={user} onLoggedOut={handleLogout} />
             <Row className="justify-content-md-center">
                 <Routes>
                     <Route
