991838e912e2830d6120c91730f58a7332c179f6
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-07-16 20:22:53 -0400

    add favorite ...movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,32 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div onClick={() => onMovieClick(movie)}>
-        <Link to={`/movies/${movie.id}`}>
-          {movie.title}
+export const MovieCard = ({ movie, user, token, onFavoriteToggle }) => {
+  const isFavorite = user.FavoriteMovies.includes(movie.id);
+
+  const handleFavorite = () => {
+    fetch(`https://moo-movies-10a7ea08abc9.herokuapp.com/users/${user.Username}/movies/${movie.id}`, {
+      method: isFavorite ? 'DELETE' : 'POST',
+      headers: { Authorization: `Bearer ${token}`}
+    })
+    .then(response => response.json())
+    .then(updatedUser => {
+      localStorage.setItem('user', JSON.stringify(updatedUser));
+      onFavoriteToggle(updatedUser);
+    })
+    .catch(e => console.error(e));
+  };
+
+  return (
+    <Card>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director}</Card.Text>
+        <Card.Text>{movie.genre}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link">Open</Button>
         </Link>
       </div>
     );
@@ -26,8 +47,11 @@ MovieCard.propTypes = {
       description: PropTypes.string.isRequired
     }).isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  token: PropTypes.string.isRequired,
+  onFavoriteToggle: PropTypes.func.isRequired
 };
+
 
 
 
