f7ff5374dd086b2818f17f5df60bea8e6cbec19d
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-07-22 18:26:29 -0400

    merge /.../navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,5 +1,47 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import MOO from '../../imgs/MOO.svg'
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+    return (
+        <Navbar bg="dark" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
                     <img 
                       src={MOO}
                       alt='cow'
                       style={{width: '100px', height: 'auto'}} 
                       />
+                    moo movies
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        { !user && (
+                            <>
+                                <Nav.Link as={Link} to="/login">
+                                    login
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/signup">
+                                    sign up
+                                </Nav.Link>
+                            </>
+                        ) }
+                        {user && (
+                            <>
+                                <Nav.Link as={Link} to="/">
+                                    home
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/profile">
+                                    profile
+                                </Nav.Link>
+                                <Nav.Link onClick={onLoggedOut}>logout</Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>  
+            </Container>
+        </Navbar>
+    );
+};
