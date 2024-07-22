ec4713d8e871f0ec94a1186208c3ad70580783df
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-07-22 18:25:09 -0400

    merge /.../movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,23 +1,51 @@
 import PropTypes from "prop-types";
-import { Card } from 'react-bootstrap';
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <Card  className="h-100 w-100" onClick={() => onMovieClick(movie)}>
-        <Card.Img variant='top' src={movie.image} />
+
+
+
+export const MovieCard = ({ movie, user, token, onFavoriteToggle }) => {
+  const isFavorite = user.favoriteMovies.includes(movie._id);
+
+  const handleFavorite = (e) => {
+  
+
+    fetch(`https://moo-movies-10a7ea08abc9.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
+      method: isFavorite ? 'DELETE' : 'POST',
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(response => response.json())
+    .then(updatedUser => {
+      localStorage.setItem('user', JSON.stringify(updatedUser));
+      onFavoriteToggle(updatedUser);
+    })
+    .catch(e => console.error(e));
+  };
+
+  return (
+    
+      <Card className="clickable-card">
+      <Link to={`/movies/${encodeURIComponent(movie._id)}`} style={{ textDecoration: 'none' }}>
+        <Card.Img variant="top" src={movie.image}  />
+        </Link>
         <Card.Body>
-        <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.director?.name}</Card.Text>
-        <Card.Text>{movie.genre?.name}</Card.Text>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.director.name}</Card.Text>
+          <Card.Text>{movie.genre.name}</Card.Text>
+          <Button variant={isFavorite ? 'danger' : 'primary'} onClick={handleFavorite}>
+            {isFavorite ? 'Unfavorite' : 'Favorite'}
+          </Button>
         </Card.Body>
       </Card>
-    );
+    
+  );
 };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     director: PropTypes.shape({
@@ -29,5 +57,12 @@ MovieCard.propTypes = {
       description: PropTypes.string.isRequired
     }).isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  user: PropTypes.object.isRequired,
+  token: PropTypes.string.isRequired,
+  onFavoriteToggle: PropTypes.func.isRequired
 };
+
+
+
+
+
