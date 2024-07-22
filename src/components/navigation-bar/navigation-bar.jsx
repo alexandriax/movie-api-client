e6553e396166f9b5304d428e67451245e77afc1b
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-07-22 18:26:13 -0400

    add logo /.../navigation-bar.jsx

---
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,5 @@
+                    <img 
+                      src={MOO}
+                      alt='cow'
+                      style={{width: '100px', height: 'auto'}} 
+                      />
