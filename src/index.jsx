2ac362f3f7e36465510e5e219777162e6c8ed753
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-08-05 14:37:09 -0400

    add redux /src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import { MainView } from "./components/main-view/main-view";
 
 import "./index.scss";
@@ -7,9 +9,11 @@ import Container from 'react-bootstrap/Container';
 //main component
 const App = () => {
     return (
-    <Container>
-      <MainView />
-    </Container>
+      <Provider store={store}>
+       <Container>
+         <MainView />
+       </Container>
+    </Provider>
     );
 };
 
