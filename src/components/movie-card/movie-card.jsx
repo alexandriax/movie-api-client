db996ee9c586582ed4ea5a957fa611db676dd0dd
alexandriax <166084006+alexandriax@users.noreply.github.com>
2024-08-05 14:33:15 -0400

    switch to id /.../movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,16 +2,11 @@ import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-
-
-
 export const MovieCard = ({ movie, user, token, onFavoriteToggle }) => {
-  const isFavorite = user.favoriteMovies.includes(movie._id);
+  const isFavorite = user.favoriteMovies.includes(movie.id); 
 
   const handleFavorite = (e) => {
-  
-
-    fetch(`https://moo-movies-10a7ea08abc9.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
+    fetch(`https://moo-movies-10a7ea08abc9.herokuapp.com/users/${user.username}/movies/${movie.id}`, {
       method: isFavorite ? 'DELETE' : 'POST',
       headers: { Authorization: `Bearer ${token}` }
     })
@@ -24,27 +19,25 @@ export const MovieCard = ({ movie, user, token, onFavoriteToggle }) => {
   };
 
   return (
-    
-      <Card className="clickable-card">
-      <Link to={`/movies/${encodeURIComponent(movie._id)}`} style={{ textDecoration: 'none' }}>
+    <Card className="clickable-card">
+      <Link to={`/movies/${encodeURIComponent(movie.id)}`} style={{ textDecoration: 'none' }}>
         <Card.Img variant="top" src={movie.image}  />
-        </Link>
-        <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <Card.Text>{movie.director.name}</Card.Text>
-          <Card.Text>{movie.genre.name}</Card.Text>
-          <Button variant={isFavorite ? 'danger' : 'primary'} onClick={handleFavorite}>
-            {isFavorite ? 'Unfavorite' : 'Favorite'}
-          </Button>
-        </Card.Body>
-      </Card>
-    
+      </Link>
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director.name}</Card.Text>
+        <Card.Text>{movie.genre.name}</Card.Text>
+        <Button variant={isFavorite ? 'danger' : 'primary'} onClick={handleFavorite}>
+          {isFavorite ? 'Unfavorite' : 'Favorite'}
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
@@ -61,6 +54,7 @@ MovieCard.propTypes = {
   token: PropTypes.string.isRequired,
   onFavoriteToggle: PropTypes.func.isRequired
 };
+
 
 
 
